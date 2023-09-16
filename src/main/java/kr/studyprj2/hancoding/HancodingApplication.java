46db62ace9b76c30a9a1ce6f4f3e21bccc40ae95
kyscodeman <metalgos2@gmail.com>
2023-09-16 12:19:47 +0900

    Initial commit

---
+++ src/main/java/kr/studyprj2/hancoding/HancodingApplication.java
@@ -0,0 +1,13 @@
+package kr.studyprj2.hancoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HancodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HancodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/studyprj2/hancoding/HancodingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.studyprj2.hancoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HancodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
