4bdf791d712c4848e8ff87dcb8ee5c63a610f23a
kyscodeman <metalgos2@gmail.com>
2023-09-16 15:31:47 +0900

    리퀘스트 파라미터 이용하여 데이터 받기

--- src/main/java/kr/studyprj2/hancoding/board/controller/MemberController.java
+++ src/main/java/kr/studyprj2/hancoding/board/controller/MemberController.java
@@ -1,6 +1,5 @@
 package kr.studyprj2.hancoding.board.controller;
 
-import kr.studyprj2.hancoding.board.dto.BoardDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,21 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/board")
-public class BoardController {
+@RequestMapping("/member")
+public class MemberController {
+    //회원가입 페이지
     @GetMapping("/save")
-    public String saveForm(){
+    public String getMemberSave(){
 
-        return "saveboard";
+        return "savemember";
     }
 
 
     @PostMapping("/save")
-    public String save(
-            //@RequestParam("boardWriter") String boadWriter 하나씩 받을떄는 리퀘스트파람, 여러개는 dto로 받는게 편함
-BoardDTO boardDTO
-    ){
+    public String postMemberSave(@RequestParam("memberEmail") String memberEmail){
 
-        return "";
+        System.out.println("memberEmail = " + memberEmail);
+        return "savemember";
     }
 }
