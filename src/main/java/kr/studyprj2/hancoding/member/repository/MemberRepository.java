b845aee79c972eb0b07c30d67e1cb8db652021e3
kyscodeman <metalgos2@gmail.com>
2023-09-19 21:11:42 +0900

    1

--- src/main/java/kr/studyprj2/hancoding/member/repository/MemberRepository.java
+++ src/main/java/kr/studyprj2/hancoding/member/repository/MemberRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
     //옵셔널 : 널방지를 위한 변수순언
-    //이메일로 디비 셀렉트. save와 다르게 셀렉트는 메서드명으로 직접 생성
-    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+
+    Optional<MemberEntity> findByMemberEmail(String memberEmail); //이메일로 디비 셀렉트. save와 다르게 셀렉트는 메서드명으로 직접 생성
 }
