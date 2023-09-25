9b2dbc8a2c0ae37691564b4a6c3652a9e879e735
kyscodeman <metalgos2@gmail.com>
2023-09-26 05:35:47 +0900

    파일 첨부를 위한 dto 변수 생성

--- src/main/java/kr/studyprj2/hancoding/board/dto/BoardDTO.java
+++ src/main/java/kr/studyprj2/hancoding/board/dto/BoardDTO.java
@@ -2,6 +2,7 @@ package kr.studyprj2.hancoding.board.dto;
 
 import kr.studyprj2.hancoding.board.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,11 @@ public class BoardDTO {
     private String boardWriter,boardPass,boardTitle,boardContents;
     private int boardHits;
     private LocalDateTime boardCreatedTime,boardUpdatedTime;
+
+    private MultipartFile boardFile; //스프링에서 지원하는 파일을 담는 변수
+    private String originalFileName; //원본파일이름
+    private String storedFileName;  //서버 저장용 파일 이름
+    private int fileAttached; // 파일 첨부여부(첨부1, 미첨부0), int 대신 bulean형은 나중에 dto에서 처리가 어려움.
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
